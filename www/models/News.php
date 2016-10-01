43d6d6463bb3027b2361d05151460069febfc114
Abdullah <alleroy@hotmail.com>
2016-10-01 13:02:02 +0200

    new commit

---
+++ www/models/News.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class News
+     extends AbstractModel
+{
+
+    public $id;
+    public $title;
+    public $text;
+
+    protected static $table = 'news';
+    protected static $class = 'News';
+
+
+
+}
