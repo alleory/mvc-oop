ba913803773ff1a1f254cba40e92d3e01c65f397
Abdullah <alleroy@hotmail.com>
2016-10-02 14:50:36 +0200

    shablonizator

--- www/views/news/all.php
+++ www/views/news/all.php
@@ -1,4 +1,3 @@
-
 <?php foreach ($items as $item): ?>
     <h1><?php echo $item->title; ?></h1>
     <div><?php echo $item->author; ?></div>
--- www/views/news/one.php
+++ www/views/news/one.php
@@ -1,2 +1,5 @@
-<h1><?php echo $item->title; ?></h1>
-<div><?php echo $item->author; ?></div>
+
+<?php foreach ($this->data['item'] as $item): ?>
+    <h1><?php echo $item->title; ?></h1>
+    <div><?php echo $item->author; ?></div>
+<?php endforeach; ?>
