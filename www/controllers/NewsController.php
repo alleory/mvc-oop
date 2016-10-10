bec8cd34ec7858a76d49c881f028e5202177914f
Abdullah <alleroy@hotmail.com>
2016-10-10 11:11:58 +0200

    htaccess

--- www/controllers/NewsController.php
+++ www/controllers/NewsController.php
@@ -5,22 +5,18 @@ class NewsController{
 
     public function actionAll()
     {
-            $art = NewsModel::findOneByColumn('title', 'borzlife12345');
 
-
-
-        /*
-        $news = News::getAll();
+        $news = NewsModel::findAll();
         $view = new View();
         $view->items = $news;
         $view->display('news/all.php');
-        */
+
     }
 
     public function actionOne()
     {
         $id = $_GET['id'];
-        $item = News::getOne($id);
+        $item = NewsModel::getOne($id);
         $view = new View();
         $view->assign('item', $item);
         $view->display('news/one.php');
